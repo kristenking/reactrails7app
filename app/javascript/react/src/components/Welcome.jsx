de24d2e781248bd58b8176a6de822b9c7a090a6e
kristenking <kristenking0110@gmail.com>
2023-03-18 22:36:52 -0400

    replaced ReactDOM.render with createRoot

--- app/javascript/react/src/components/Welcome.jsx
+++ app/javascript/react/src/components/Welcome.jsx
@@ -1,17 +1,23 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 import QuestionList from './QuestionList';
 
 function Welcome() {
-  return (
-    <div className='container'> 
-      <h1>Welcome to React Rails 7 App</h1>
-      <QuestionList />
-    </div>
- 
-  )
+    return (
+        <div className='container'>
+            <h1>Welcome to React Rails 7 App</h1>
+            <QuestionList />
+        </div>
+
+    )
 }
 
-document.addEventListener('DOMContentLoaded', () => {ReactDOM.render(<Welcome />, document.getElementById('welcome'))});
+const root = ReactDOM.createRoot(document.getElementById('welcome'));
+root.render(
+    <React.StrictMode>
+        <Welcome />
+    </React.StrictMode>
+);
+
 
 export default Welcome
