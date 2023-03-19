df1f640a6754a0fa018bce453afd9f12d7566a74
kristenking <kristenking0110@gmail.com>
2023-03-19 00:13:21 -0400

    created rails API endpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating questions"
+questions = Question.create([
+    {
+        title: "What is Ruby's method for creating arrays?",
+        tag: "Ruby"
+    },
+    {
+        title: "How do you define a method in Ruby?",
+        tag: "Ruby"
+    },
+    {
+        title: "What is a block in Ruby?",
+        tag: "Ruby"
+    },
+    {
+        title: "How do you install a gem in Ruby?",
+        tag: "Ruby"
+    },
+    {
+        title: "What is the difference between 'puts' and 'print' in Ruby?",
+        tag: "Ruby"
+    },
+    {
+        title: "How do you handle errors in Ruby?",
+        tag: "Ruby"
+    },
+    {
+        title: "What is a class in Ruby?",
+        tag: "Ruby"
+    }
+])
+puts "Created #{questions.count} questions"
