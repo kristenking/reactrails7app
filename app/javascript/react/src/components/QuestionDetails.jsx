916825f0d9dabcdcf284861f780f698b892b0987
kristenking <kristenking0110@gmail.com>
2023-03-18 22:47:12 -0400

    added like button and like count

--- app/javascript/react/src/components/QuestionDetails.jsx
+++ app/javascript/react/src/components/QuestionDetails.jsx
@@ -1,12 +1,22 @@
 import * as React from 'react';
+import { useState } from 'react';
 import * as ReactDOM from 'react-dom';
 
 function QuestionDetails(props) {
+
+    const [likeCount, setLikeCount] = useState(0)
+    
   return (
     <div className='card rounded-10 mt-3' >
     <div className='card-body'>
         <h3 className='card-title'>{props.question.title}</h3>
-        <p className="lead"><span className='badge bg-danger'>{props.question.tag}</span></p>
+        <p className="lead">
+            <span className='badge bg-danger'>{props.question.tag}</span>
+            </p>
+    <button className='btn btn-primary mt-1' onClick={() => setLikeCount(likeCount + 1)}>Like</button>
+    {
+        likeCount > 0 && <span className='badge bg-primary ms-1'>{likeCount}</span>
+    }
     </div>
 </div>
   )
