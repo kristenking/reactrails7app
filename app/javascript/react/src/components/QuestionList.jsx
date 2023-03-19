dd763269c8ca7f9c2194de69f1a6e1d716d449c4
kristenking <kristenking0110@gmail.com>
2023-03-19 00:29:07 -0400

    fetched questions data from API

--- app/javascript/react/src/components/QuestionList.jsx
+++ app/javascript/react/src/components/QuestionList.jsx
@@ -1,46 +1,64 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import * as ReactDOM from 'react-dom';
 import QuestionDetails from './QuestionDetails';
 
 
 function QuestionList() {
-    questionsList = [
-        {
-            id: 1,
-            title: "What is Ruby's method for creating arrays?",
-            tag: "Ruby"
-        },
-        {
-            id: 2,
-            title: "How do you define a method in Ruby?",
-            tag: "Ruby"
-        },
-        {
-            id: 3,
-            title: "What is a block in Ruby?",
-            tag: "Ruby"
-        },
-        {
-            id: 4,
-            title: "How do you install a gem in Ruby?",
-            tag: "Ruby"
-        },
-        {
-            id: 5,
-            title: "What is the difference between 'puts' and 'print' in Ruby?",
-            tag: "Ruby"
-        },
-        {
-            id: 6,
-            title: "How do you handle errors in Ruby?",
-            tag: "Ruby"
-        },
-        {
-            id: 7,
-            title: "What is a class in Ruby?",
-            tag: "Ruby"
-        }
-    ]
+    const [questionsList, setQuestionsList] = useState([]);
+    const questionUrl = 'http://localhost:3000/api/v1/questions';
+
+    const fetchQuestionList = () => {
+        fetch(questionUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                setQuestionsList(data)
+            }
+            )
+    }
+
+    useEffect(() => {
+        fetchQuestionList()
+    }, [])
+
+    // questionsList = [
+    //     {
+    //         id: 1,
+    //         title: "What is Ruby's method for creating arrays?",
+    //         tag: "Ruby"
+    //     },
+    //     {
+    //         id: 2,
+    //         title: "How do you define a method in Ruby?",
+    //         tag: "Ruby"
+    //     },
+    //     {
+    //         id: 3,
+    //         title: "What is a block in Ruby?",
+    //         tag: "Ruby"
+    //     },
+    //     {
+    //         id: 4,
+    //         title: "How do you install a gem in Ruby?",
+    //         tag: "Ruby"
+    //     },
+    //     {
+    //         id: 5,
+    //         title: "What is the difference between 'puts' and 'print' in Ruby?",
+    //         tag: "Ruby"
+    //     },
+    //     {
+    //         id: 6,
+    //         title: "How do you handle errors in Ruby?",
+    //         tag: "Ruby"
+    //     },
+    //     {
+    //         id: 7,
+    //         title: "What is a class in Ruby?",
+    //         tag: "Ruby"
+    //     }
+    // ]
 
     return (
         <div className='row'>
