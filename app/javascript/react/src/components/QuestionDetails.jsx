e18120ce66cb1ce1d3fe0ed9f899f2404c1db926
kristenking <kristenking0110@gmail.com>
2023-03-19 13:32:20 -0400

    fetched like/dislike count from client to api

--- app/javascript/react/src/components/QuestionDetails.jsx
+++ app/javascript/react/src/components/QuestionDetails.jsx
@@ -2,10 +2,40 @@ import * as React from 'react';
 import { useState } from 'react';
 import * as ReactDOM from 'react-dom';
 
+
+
 function QuestionDetails(props) {
 
-  const [likeCount, setLikeCount] = useState(0)
-  const [dislikeCount, setDislikeCount] = useState(0)
+  const [likeCount, setLikeCount] = useState(props.question.likes_count);
+  const [dislikeCount, setDislikeCount] = useState(props.question.dislikes_count);
+
+  const updateQuestionCounter = (data) => {
+    fetch(`http://localhost:3000/api/v1/questions/${props.question.id}/update_counter`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+    })
+    .catch ((error) => {
+      console.log(error)
+    })
+  }
+
+  const handleLikeClick = () => {
+    setLikeCount(likeCount + 1);
+    updateQuestionCounter({ count_for: 'like'});
+  };
+
+  const handleDislikeClick = () => {
+    setDislikeCount(dislikeCount + 1);
+    updateQuestionCounter({ count_for: 'dislike' }); 
+  };
+
 
   return (
     <div className='card rounded-10 mt-3' >
@@ -14,15 +44,14 @@ function QuestionDetails(props) {
         <p className="lead">
           <span className='badge bg-danger'>{props.question.tag}</span>
         </p>
-        <button type="button" className="btn btn-primary position-relative" onClick={() => setLikeCount(likeCount + 1)} style={{ marginRight: 1 + "em" }}>
+        <button type="button" className="btn btn-primary position-relative" onClick={handleLikeClick} style={{ marginRight: 1 + "em" }}>
           Like
-
           {
             likeCount > 0 && <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"> {likeCount}
             </span>
           }
         </button>
-        <button type="button" className="btn btn-primary position-relative" onClick={() => setDislikeCount(dislikeCount + 1)}>
+        <button type="button" className="btn btn-primary position-relative" onClick={handleDislikeClick}>
           Dislike
           {
             dislikeCount > 0 && <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"> {dislikeCount}
