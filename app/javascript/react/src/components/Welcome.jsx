86e8a4d70f6c307ad46952faf1d605e0f82fa205
kristenking <kristenking0110@gmail.com>
2023-04-03 01:41:23 -0400

    created popup module for login/signup

--- app/javascript/react/src/components/Welcome.jsx
+++ app/javascript/react/src/components/Welcome.jsx
@@ -3,29 +3,43 @@ import * as ReactDOM from 'react-dom/client';
 import Login from './sessions/Login';
 import Signup from './sessions/Signup';
 import Logout from './sessions/Logout';
+import AuthModule from './sessions/AuthModule';
 import QuestionList from './QuestionList';
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 function Welcome() {
-    return (
-        <div className='container mt-5'>
-            <h1 style={{color:"#FEBF01"}}>Welcome to React Rails 7 App</h1>
-            <Login />
-            <Signup />
-            <Logout />
-            <QuestionList />
-        </div>
+  const [showAuthModule, setShowAuthModule] = React.useState(false);
 
-    )
+  const toggleAuthModule = () => {
+    setShowAuthModule(!showAuthModule);
+  };
+
+  return (
+    <div className='container mt-5'>
+      <div className="d-flex align-items-center">
+        <h1 style={{ color: "#FEBF01" }}>Welcome to React Rails 7 App</h1>
+        <FontAwesomeIcon
+          icon={faUser}
+          className="ml-3"
+          style={{ cursor: "pointer" }}
+          onClick={toggleAuthModule}
+        />
+      </div>
+      <Login />
+      <Signup />
+      <Logout />
+      {showAuthModule && <AuthModule />}
+      <QuestionList />
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('welcome'));
 root.render(
-    <React.StrictMode>
-        <Welcome />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Welcome />
+  </React.StrictMode>
 );
 
-
-export default Welcome
+export default Welcome;
---
+++ app/javascript/react/src/components/sessions/AuthModule.jsx
@@ -0,0 +1,163 @@
+import React, { useState } from 'react';
+import Logout from './Logout';
+
+const AuthModule = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [formFields, setFormFields] = useState({
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+  });
+
+  const csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (isSignUp) {
+      // Sign-up logic
+      try {
+        const response = await fetch("/api/v1/users", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-CSRF-Token": csrf,
+          },
+          body: JSON.stringify({
+            user: {
+              email: formFields.email,
+              password: formFields.password,
+              password_confirmation: formFields.passwordConfirmation,
+            },
+          }),
+        });
+
+        if (response.ok) {
+          alert("Sign up successful!");
+          // Redirect the user to the home page
+          window.location.href = "/";
+        } else {
+          // Handle errors (e.g., display an error message)
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    } else {
+      // Login logic
+      try {
+        const response = await fetch("/api/v1/users/sign_in", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-CSRF-Token": csrf,
+          },
+          body: JSON.stringify({ user: { email: formFields.email, password: formFields.password } }),
+        });
+
+        if (response.ok) {
+          setIsLoggedIn(true); // Set authentication status to true
+        } else {
+          // Handle errors (e.g., display an error message)
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+  };
+
+  const handleFormFields = (event) => {
+    setFormFields({ ...formFields, [event.target.name]: event.target.value });
+  };
+
+  const switchForm = () => {
+    setIsSignUp(!isSignUp);
+  };
+
+  // Conditionally render different components based on authentication status
+  return (
+    <>
+      {!isLoggedIn ? (
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-6">
+              <div className="card mt-5">
+                <div className="card-body">
+                  <h1 className="card-title text-center">{isSignUp ? 'Sign Up' : 'Login'}</h1>
+                  <form onSubmit={handleSubmit}>
+                    {isSignUp && (
+                      <div className="form-group">
+                        <label className="form-label mt-3 mb-3">Name</label>
+                        <input
+                          type="text"
+                          className="form-control form-control-lg rounded-3"
+                          value={formFields.name}
+                          onChange={handleFormFields}
+                          name="name"
+                          required
+                        />
+                      </div>
+                    )}
+                    <div className="form-group">
+                      <label className="form-label mt-3 mb-3">Email</label>
+                      <input
+                        type="email"
+                        className="form-control form-control-lg rounded-3"
+                        value={formFields.email}
+                        onChange={handleFormFields}
+                        name="email"
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label className="form-label mt-3 mb-3">Password</label>
+                      <input
+                    type="password"
+                    className="form-control form-control-lg rounded-3"
+                    value={formFields.password}
+                    onChange={handleFormFields}
+                    name="password"
+                    required
+                  />
+                </div>
+                {isSignUp && (
+                  <div className="form-group">
+                    <label className="form-label mt-3 mb-3">Confirm Password</label>
+                    <input
+                      type="password"
+                      className="form-control form-control-lg rounded-3"
+                      value={formFields.passwordConfirmation}
+                      onChange={handleFormFields}
+                      name="passwordConfirmation"
+                      required
+                    />
+                  </div>
+                )}
+                <button type="submit" className="btn btn-primary w-100 mt-3">
+                  {isSignUp ? 'Sign Up' : 'Login'}
+                </button>
+              </form>
+              <div className="text-center mt-3">
+                <small>
+                  {isSignUp ? 'Already have an account?' : "Don't have an account?"}{' '}
+                  <button type="button" className="btn btn-link" onClick={switchForm}>
+                    {isSignUp ? 'Login' : 'Sign Up'}
+                  </button>
+                </small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <>
+      <p>You are logged in!</p>
+      <Logout setIsLoggedIn={setIsLoggedIn} />
+    </>
+  )}
+</>
+);
+};
+
+export default AuthModule;
