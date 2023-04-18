37a6c9c84cc67029293427ef340b89cff78788a8
kristenking <kristenking0110@gmail.com>
2023-04-18 12:10:19 -0400

    updated session controler

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,5 +2,11 @@ class Api::V1::SessionsController < Devise::SessionsController
     protect_from_forgery with: :null_session
   
     respond_to :json
+
+    def create
+      self.resource = warden.authenticate!(auth_options)
+      sign_in(resource_name, resource)
+      render json: { message: 'Signed in successfully.' }, status: :ok
+    end
   end
   
